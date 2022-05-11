be758478f84b0b60475f258932972eb1191d24d7
Iulian ANDREI <adri3l.ro@gmail.com>
2022-05-11 13:55:55 +0300

    Added solution for Flask basics exercise.

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def hello():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
         # https://flask.palletsprojects.com/en/2.1.x/quickstart/#the-request-object
-        print('You made it, almost!')
+        print(f'''{request.form['author']}: "{request.form['quote']}"''')
+        return redirect(url_for('view',
+                                author=request.form['author'],
+                                quote=request.form['quote']))
     return render_template('add.html')
+
+
+@app.route('/view/', methods=['GET'])
+@app.route('/view/<author>/<quote>', methods=['GET'])
+def view(author=None, quote=None):
+    return render_template('view.html',
+                           author=author,
+                           quote=quote)
+
+
+@app.route('/contact', methods=['GET'])
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
